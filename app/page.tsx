7a1d3b2a8afe90fc4693fcfedbaec1c03f5821fd
reda baskiki <baskikireda.rb@gmail.com>
2024-11-07 14:57:43 +0100

    Eslint Prettier settup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-  <h1 className="text-3kl text-violet-700 font-black"> welcome to the world next.js</h1>
+    <h1 className="text-3kl text-violet-700 font-black">
+     
+      welcome to the world next.js
+    </h1>
   );
 }
