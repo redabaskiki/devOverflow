f6f461ca01b2556ebca78103a9f933cdd1e57deb
reda baskiki <baskikireda.rb@gmail.com>
2024-11-13 16:05:30 +0100

    Question Editor

--- app/(root)/ask-question.tsx/page.tsx
+++ app/(root)/ask-question.tsx/page.tsx
@@ -1,9 +1,15 @@
+import QuestionForm from '@/components/forms/QuestionForm';
 import React from 'react'
 
 const  AskQuestion = () => {
   return (
-    <div>page</div>
-  )
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
 }
 
 export default AskQuestion;
