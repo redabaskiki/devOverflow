f47d8bcea4556149a60d9c2373b144391ac24a5e
reda baskiki <baskikireda.rb@gmail.com>
2024-11-13 13:55:53 +0100

    QuestionCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { auth, signOut } from "@/auth";
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
@@ -8,35 +9,46 @@ import Link from "next/link";
 // Home.tsx 
  
  const questions = [
-  {
-    _id: "1",
-    title: "How to learn React?",
-    description: "I want to learn React, can anyone help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "JavaScript" },
-    ],
-    author: { _id: "1", name: "John Doe" },
-    upvotes: 10,
-    answers: 5,
-    views: 100,
-    createdAt: new Date(),
-  },
-  {
-    _id: "2",
-    title: "How to learn JavaScript?",
-    description: "I want to learn JavaScript, can anyone help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "JavaScript" },
-    ],
-    author: { _id: "1", name: "John Doe" },
-    upvotes: 10,
-    answers: 5,
-    views: 100,
-    createdAt: new Date(),
-  },
-];
+   {
+     _id: "1",
+     title: "How to learn React?",
+     description: "I want to learn React, can anyone help me?",
+     tags: [
+       { _id: "1", name: "React" },
+       { _id: "2", name: "JavaScript" },
+     ],
+     author: {
+       _id: "1",
+       name: "John Doe",
+       image:
+         "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+     },
+     upvotes: 10,
+     answers: 5,
+     views: 100,
+     createdAt: new Date(),
+   },
+   {
+     _id: "2",
+     title: "How to learn JavaScript?",
+     description: "I want to learn JavaScript, can anyone help me?",
+     tags: [
+       { _id: "1", name: "React" },
+       { _id: "2", name: "JavaScript" },
+     ],
+     author: {
+       _id: "1",
+       name: "John Doe",
+       image:
+         "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+     },
+
+     upvotes: 10,
+     answers: 5,
+     views: 100,
+     createdAt: new Date("2021-09-01"),
+   },
+ ];
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
@@ -71,7 +83,9 @@ const Home = async ({ searchParams }: SearchParams) => {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
-          <h1 key={question._id}>{question.title}</h1>
+ 
+          <QuestionCard key={question._id} question={question} />
+     
         ))}
       </div>
     </>
