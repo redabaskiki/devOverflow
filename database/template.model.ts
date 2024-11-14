7f130cd54a67fd3aae5aca5743c719d0ad3525f1
reda baskiki <baskikireda.rb@gmail.com>
2024-11-14 14:53:13 +0100

    question model and template model (replace)

---
+++ database/template.model.ts
@@ -0,0 +1,5 @@
+import { model, models, Schema, Types } from "mongoose";
+export interface IModel {}
+const ModelSchema = new Schema<IModel>({}, { timestamps: true });
+const Model = models?.Account || model<IModel>("Account", ModelSchema);
+export default Model;
